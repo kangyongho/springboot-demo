2df04123f7ffd381530ee1917456391e982b2ae8
Daniel Kang <postmirlang2@gmail.com>
2016-11-24 11:56:58 +0900

    spring boot demo project initial commit

---
+++ src/main/java/net/ddns/HelloController.java
@@ -0,0 +1,29 @@
+package net.ddns;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by NPOST on 2016-11-24.
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String index(Locale locale, Model model) {
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formatteDate = dateFormat.format(date);
+
+        model.addAttribute("servertime", formatteDate);
+        model.addAttribute("name", "Spring from daniel");
+
+        return "hello";
+    }
+}
