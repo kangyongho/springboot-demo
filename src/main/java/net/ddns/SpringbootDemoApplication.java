7424b1f165aa6600981937a5a350e4da84064f19
Daniel Kang <postmirlang2@gmail.com>
2016-11-28 18:16:18 +0900

    Spring Boot war 파일 생성 클래스 통합

--- src/main/java/net/ddns/SpringbootDemoApplication.java
+++ src/main/java/net/ddns/SpringbootDemoApplication.java
@@ -2,9 +2,16 @@ package net.ddns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
